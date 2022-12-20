d6a588ad1f9be43ddcac178a652db2e3b57529a5
mc1496 <maxx.raxy@gmail.com>
2022-12-20 16:00:36 -0500

    script to install python 3.9

---
+++ xam/amazon-linux2-install-python3p9.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+sudo yum -y install openssl-devel bzip2-devel libffi-devel wget
+
+#https://www.python.org/downloads/source/
+wget https://www.python.org/ftp/python/3.9.16/Python-3.9.16.tar.xz
+tar xvf Python-3.9.16.tar.xz
+
+cd Python-*/
+./configure --enable-optimizations
+sudo make altinstall
+
+python3.9 --version
+python3.9 -m pip install --upgrade pip
+
+echo "use python3.9 -m venv env"
+#NOTE: voclabs:~/environment $ ls -al /usr/bin/python*
+#lrwxrwxrwx 1 root root   16 Dec 13 12:44 /usr/bin/python3-config -> python3.7-config
+#lrwxrwxrwx 1 root root    9 Dec 13 12:36 /usr/bin/python3 -> python3.7
+
