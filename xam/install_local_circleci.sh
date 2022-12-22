6fcaf0154f07fd04cc187442fbe838a814e5d174
mc1496 <maxx.raxy@gmail.com>
2022-12-22 15:11:07 +0000

    added circleci CLI installation results/log

--- xam/install_local_circleci.sh
+++ xam/install_local_circleci.sh
@@ -15,4 +15,28 @@ curl -fLSs "$URL" | sudo bash
 #we need API token and setup before using CLI
 #circleci setup
 
+#LOGS
+# voclabs:~/environment/DevOps_Microservices/xam (master) $ ./install_local_circleci.sh 
+# Starting installation.
+# Installing CircleCI CLI v0.1.22875
+# Installing to /usr/local/bin
+# An error occured installing the tool.
+# The contents of the directory /tmp/tmp.Vyn35w2MOK have been left in place to help to debug the issue.
 
+# voclabs:~/environment/DevOps_Microservices/xam (master) $ sudo ls /tmp/tmp.Vyn35w2MOK/
+# api  circleci  md_docs
+
+# voclabs:~/environment $ sudo ls /tmp/tmp.Vyn35w2MOK/
+# api  circleci  md_docs
+# voclabs:~/environment $ sudo ls /tmp/tmp.Vyn35w2MOK/api
+# graphql
+# voclabs:~/environment $ sudo ls /tmp/tmp.Vyn35w2MOK/md_docs
+# LICENSE
+# voclabs:~/environment $ 
+
+
+# voclabs:~/environment/DevOps_Microservices/xam (master) $ ls /usr/local/bin/c*                                                                   
+# /usr/local/bin/circleci
+# voclabs:~/environment/DevOps_Microservices/xam (master) $ circleci
+# 2022/12/22 14:59:43 
+# ...
